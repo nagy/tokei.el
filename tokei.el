25b872502041df41892c778693f5192ad9e9c4bf
Daniel Nagy <danielnagy@posteo.de>
2025-06-21 16:30:00 +0200

    style: reindent file

--- tokei.el
+++ tokei.el
@@ -78,24 +78,24 @@
 (defun tokei--make-data ()
   "Return newly created, pre-sorted tokei data for this directory."
   (sort
-    (json-parse-string
-      (with-temp-buffer
-        (if (zerop (call-process tokei-program nil '(t nil) nil "--output=json" ))
+   (json-parse-string
+    (with-temp-buffer
+      (if (zerop (call-process tokei-program nil '(t nil) nil "--output=json" ))
           (buffer-string)
-          ""))
-      :object-type 'alist
-      :array-type 'list)
-    #'tokei--sort-predicate))
+        ""))
+    :object-type 'alist
+    :array-type 'list)
+   #'tokei--sort-predicate))
 
 (defun tokei--sort-predicate (elem1 elem2)
   "A predicate to compare ELEM1 and ELEM2 by num of code and then name."
   (let-alist `((el1 . ,elem1) (el2 . ,elem2))
     (let ((numcode1 (or .el1.code .el1.stats.code))
-         (numcode2 (or .el2.code .el2.stats.code))
-         (name1 (or .el1.name (car  elem1)))
-         (name2 (or .el2.name (car  elem2))))
+          (numcode2 (or .el2.code .el2.stats.code))
+          (name1 (or .el1.name (car elem1)))
+          (name2 (or .el2.name (car elem2))))
       (if (= numcode1 numcode2)
-        (string-lessp name1 name2)
+          (string-lessp name1 name2)
         (> numcode1 numcode2)))))
 
 (defun tokei--formatted-stats (code comments)
@@ -103,87 +103,87 @@
 
 Takes CODE and COMMENTS entries."
   (concat
-    (cond
-      ((>= 9 code) "    ")
-      ((>= 99 code) "   ")
-      ((>= 999 code) "  ")
-      ((>= 9999 code) " "))
-    (propertize (number-to-string code) 'face 'tokei-num-code-face)
-    tokei-separator
-    (cond
-      ((>= 9 comments) "    ")
-      ((>= 99 comments) "   ")
-      ((>= 999 comments) "  ")
-      ((>= 9999 comments) " "))
-    (propertize (number-to-string comments) 'face 'tokei-num-comments-face)))
+   (cond
+    ((>= 9 code) "    ")
+    ((>= 99 code) "   ")
+    ((>= 999 code) "  ")
+    ((>= 9999 code) " "))
+   (propertize (number-to-string code) 'face 'tokei-num-code-face)
+   tokei-separator
+   (cond
+    ((>= 9 comments) "    ")
+    ((>= 99 comments) "   ")
+    ((>= 999 comments) "  ")
+    ((>= 9999 comments) " "))
+   (propertize (number-to-string comments) 'face 'tokei-num-comments-face)))
 
 (defun tokei--get-sorted-files (json)
   "Retrieve multiple sorted entries.
 
 Data is provided via the JSON argument."
   (sort
-    (copy-sequence (alist-get 'reports json))
-    #'tokei--sort-predicate))
+   (copy-sequence (alist-get 'reports json))
+   #'tokei--sort-predicate))
 
 (defun tokei--imenu-create-index-function ()
   "Create an imenu index for tokei-mode buffers."
   `(("Languages"
-      ,@(cl-loop for lang in (oref magit-root-section children)
-          collect
-          (cons (oref lang value) (oref lang start))))
-     ("Files"
-       ,@(mapcar
-           (lambda (file)
-             (cons
-               (string-remove-prefix "./" (oref file value))
-               (oref file start)))
-           (flatten-tree
-             (cl-loop for lang in (oref magit-root-section children)
-               collect
-               (oref lang children)))))))
+     ,@(cl-loop for lang in (oref magit-root-section children)
+                collect
+                (cons (oref lang value) (oref lang start))))
+    ("Files"
+     ,@(mapcar
+        (lambda (file)
+          (cons
+           (string-remove-prefix "./" (oref file value))
+           (oref file start)))
+        (flatten-tree
+         (cl-loop for lang in (oref magit-root-section children)
+                  collect
+                  (oref lang children)))))))
 
 (define-derived-mode tokei-mode magit-section-mode "Tokei"
   "Tokei mode."
   :interactive nil
   :group 'tokei
   (setq-local
-    tokei-data (tokei--make-data)
-    revert-buffer-function (lambda (&rest _) (tokei-mode))
-    bookmark-make-record-function #'tokei--bookmark-make-record-function
-    imenu-create-index-function #'tokei--imenu-create-index-function
-    header-line-format (when tokei-use-header-line
-                         (concat "File" (propertize " " 'display '(space :align-to center))
-                           "Code" tokei-separator "Comments")))
+   tokei-data (tokei--make-data)
+   revert-buffer-function (lambda (&rest _) (tokei-mode))
+   bookmark-make-record-function #'tokei--bookmark-make-record-function
+   imenu-create-index-function #'tokei--imenu-create-index-function
+   header-line-format (when tokei-use-header-line
+                        (concat "File" (propertize " " 'display '(space :align-to center))
+                                "Code" tokei-separator "Comments")))
   (let ((inhibit-read-only t))
     (erase-buffer)
     (magit-insert-section (tokei-root)
       (cl-loop for lang in tokei-data
-        for langname = (format "%s" (car lang))
-        unless (string= langname "Total")
-        do
-        (magit-insert-section (tokei-language langname)
-          (magit-insert-heading (concat
-                                  (propertize langname 'face 'magit-section-heading)
-                                  (propertize " " 'display '(space :align-to center))
-                                  (tokei--formatted-stats (alist-get 'code lang) (alist-get 'comments lang))
-                                  "\n"))
-          (cl-loop for file in (tokei--get-sorted-files lang)
-            do
-            (let-alist file
-              (magit-insert-section (tokei-file .name)
-                (insert
-                  (string-remove-prefix "./" .name)
-                  (propertize " " 'display '(space :align-to center))
-                  (tokei--formatted-stats .stats.code .stats.comments)
-                  "\n"))))
-          (insert "\n"))))
+               for langname = (format "%s" (car lang))
+               unless (string= langname "Total")
+               do
+               (magit-insert-section (tokei-language langname)
+                 (magit-insert-heading (concat
+                                        (propertize langname 'face 'magit-section-heading)
+                                        (propertize " " 'display '(space :align-to center))
+                                        (tokei--formatted-stats (alist-get 'code lang) (alist-get 'comments lang))
+                                        "\n"))
+                 (cl-loop for file in (tokei--get-sorted-files lang)
+                          do
+                          (let-alist file
+                            (magit-insert-section (tokei-file .name)
+                              (insert
+                               (string-remove-prefix "./" .name)
+                               (propertize " " 'display '(space :align-to center))
+                               (tokei--formatted-stats .stats.code .stats.comments)
+                               "\n"))))
+                 (insert "\n"))))
     (goto-char (point-min))))
 
 (defun tokei--bookmark-make-record-function ()
   "A function to be used as `bookmark-make-record-function'."
   `(,(concat "tokei: " (abbreviate-file-name default-directory))
-     (handler . tokei-bookmark-jump)
-     (filename . ,(abbreviate-file-name default-directory))))
+    (handler . tokei-bookmark-jump)
+    (filename . ,(abbreviate-file-name default-directory))))
 
 ;;;###autoload
 (defun tokei-bookmark-jump (bm)
